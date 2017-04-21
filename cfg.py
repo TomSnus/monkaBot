6a270d27152677a557ab78ff6ab1db7bc25a7a3f
TomSnus <senteswins@gmail.com>
2017-04-21 13:18:01 +0200

    Added Commands Class

--- cfg.py
+++ cfg.py
@@ -2,5 +2,5 @@ HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "monkabot"
 PASS = "oauth:zp0fltqis1cqsdvpzw8bwgtt47dcz8"
-CHAN = "senteswins"
+CHAN = "marckotobeloo"
 RATE = (20/30) # messages per second
